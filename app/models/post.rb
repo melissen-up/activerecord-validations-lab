5449c09cdc08abc486904b534e47354ab59d43c4
Melissa Faulner <melissa.faulner@gmail.com>
2021-01-18 17:01:31 -0500

    commit tree 6c6e45601f435a77daa1b7c9e9ec65d1bee7cfbf

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :clickbait_validation
+
+    CLICKBAIT = [
+        /Won't Believe/,
+        /Secret/,
+        /Top [0-100]/,
+        /Guess/
+    ]
+
+    def clickbait_validation
+        if CLICKBAIT.none?{|arr| arr.match title }
+            errors.add(:title, "must be clickbait")
+        end
+    end
 end
